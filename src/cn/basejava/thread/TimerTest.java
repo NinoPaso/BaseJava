adce37a9c42ee89d5c289ff5a1dbc1e78623e012
SmellyPig <2080630697@qq.com>
2020-03-04 22:13:53 +0800

    线程基础学习提交

---
+++ src/cn/basejava/thread/TimerTest.java
@@ -0,0 +1,26 @@
+package cn.basejava.thread;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @ClassName TimerTest
+ * @Description TODO
+ * @Author 86133
+ * @Date 2020/3/1 18:28
+ * @Version 1.0
+ **/
+public class TimerTest {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new MyTask(),1000,2000);
+    }
+}
+
+class MyTask extends TimerTask{
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+        System.out.println("----end----");
+    }
+}
